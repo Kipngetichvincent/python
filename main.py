4b1f1fb6b67b009424fea04f63fa1e9d99963e18
Kipngetichvincent <kipngetichvincent773@gmail.com>
2021-12-01 18:02:26 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+print("hello, world")
+
+# integer
+a = 2
+print(a)
+
+# float point
+pi = 3.14
+print(pi)
+
+# string
+c = "A"
+print(c)
+
+# string
+name = "John, Doe"
+print(name)
+
+# boolean
+q = "true"
+print(q)
+
+a = 2
+print(type(a))
+
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+a = b = c = 1
+print(a, b, c)
+
+x = y = [7, 8, 9]
+x[0] = 13
+print(y)
+
+def my_function():
+    a = 2
+    return
+print(my_function())
+
+names = ['Alice', 'Bob', 'Craig', 'Daina', 'Eric']
+print(names[0])
+print(names[2])
+
+print(names[-1])
+print(names[-4])
+names[0] = 'Ann'
+print(names)
+
+names[0] = 'Ann'
+print(names)
+
+names = ['Alice', 'Bob', 'Craig', 'Diana', 'Eric']
+names.append("Sia")
+print(names)
+
+names = ['Alice', 'Bob', 'Craig', 'Diana', 'Eric', 'Sia']
+names.insert(1,"Nikki")
+print(names)
+
+names = ['Alice', 'Bob', 'Craig', 'Diana', 'Eric', 'Sia']
+names.remove("Bob")
+print(names)
+
+state_capitals = {
+    'Arkansas': 'Little Rock',
+    'Colorado': 'Denver',
+    'California': 'Secramento',
+    'Georgia': 'Atlanta',
+}
+
+state_capitals['Arkansas'] = 'Little Rock'
+state_capitals['California'] = 'Sacramento'
+state_capitals['Colorado'] = 'Denver'
+state_capitals['Georgia'] = 'Atlanta'
+
+state_capitals['Alabama']
